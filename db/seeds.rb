1fd07f27caf7ccf99f0506766d27075ebd81e66c
Angus Spawton-Rice <adspawtonrice@gmail.com>
2022-10-15 16:38:02 +0100

    DB seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+100.times do
+  Article.new(
+    title: Faker::Quote.famous_last_words,
+    author: Faker::FunnyName.two_word_name,
+    content: Faker::Lorem.paragraph(sentence_count: 14),
+    views: rand(1..1111)
+  ).save
+end
